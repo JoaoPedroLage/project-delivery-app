b3a545548ac57dfe7d2a3555fb63b3a77a2e45ad
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-01 16:03:12 -0300

    feat: Creating App

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,51 @@
 const express = require('express');
+const errorMiddleware = require('./middlewares/errorHandler');
+const loginRouter = require('./routes/loginRouter');
 
-const app = express();
+class App {
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routes();
+    this.errorHandler();
+  }
 
-app.get('/coffee', (_req, res) => res.status(418).end());
+  startServer(PORT= 3001) {
+    this.app.listen(
+      PORT,
+      () => console.log(`Server running here 👉 http://localhost:${PORT}`),
+    );
+  }
 
-module.exports = app;
+  config() {
+    const accessControl = (_req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
+      res.header('Access-Control-Allow-Headers', '*');
+      next();
+    };
+
+    this.app.use(accessControl);
+    this.app.use(express.json());
+  }
+
+  // addRouter(router: Router) {
+  //   this.app.use(router);
+  // }
+
+  routes() {
+    this.app.use('/login', loginRouter);
+  }
+
+  getApp() {
+    return this.app;
+  }
+
+    
+  errorHandler() {
+    this.app.use(errorMiddleware);
+  }
+
+}
+
+module.exports = App;
