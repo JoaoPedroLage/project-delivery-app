2569d8734c1c0e5fbe506a87601e225e3bc889da
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-01 15:58:10 -0300

    feat: Creating SalesProduct model

---
+++ back-end/src/database/models/SalesProducts.js
@@ -0,0 +1,43 @@
+// const Sale = require("./Sale");
+
+const SalesProducts = (sequelize, DataTypes) => {
+  const SalesProducts = sequelize.define('SalesProducts', {
+    sale_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    ṕroduct_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+    quantity: {
+      allowNull: false,
+      type: DataTypes.INTEGER
+    },
+  }, {
+    underscored: true,
+    sequelize,
+    modelName: 'SalesProducts',
+    tableName: 'salesProducts',
+    timestamps: false,
+  });
+
+  SalesProducts.associate = (models) => {
+    models.Sale.belongsToMany(models.Product, {
+      as: 'products',
+      through: SalesProducts,
+      foreignKey: 'sale_id',
+      otherKey: 'product_id',
+    });
+    models.Product.belongsToMany(models.Sale, {
+      as: 'sales',
+      through: SalesProducts,
+      foreignKey: 'product_id',
+      otherKey: 'sale_id',
+    });
+  }
+  
+  return SalesProducts;
+}
+
+module.exports = SalesProducts;
