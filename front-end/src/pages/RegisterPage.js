27ebe0c3bd54918b9b8e462d6ab69f2b696a3a2d
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-03 18:20:40 -0300

    feat: Adding inputs and btn to register page

--- front-end/src/pages/RegisterPage.js
+++ front-end/src/pages/RegisterPage.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Button, Form } from 'react-bootstrap';
+import EmailInput from '../components/register/EmailInput';
+import PasswordInput from '../components/register/PasswordInput';
+import NameInput from '../components/register/NameInput';
 
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+
+    <div className="register-page-container">
+      <div className="register-items-container">
+
+        <Form className="register-form" onSubmit={ (e) => onSubmitLogin(e) }>
+          <NameInput />
+          <EmailInput />
+          <PasswordInput />
+          <Button
+            data-testid="common_register__button-register"
+            variant="success"
+            type="button"
+            onClick={ () => console.log('cadastrar') }
+          >
+            Register
+          </Button>
+        </Form>
+      </div>
+    </div>
   );
 }
