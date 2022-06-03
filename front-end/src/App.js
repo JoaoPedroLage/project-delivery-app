b3383b732d8c8af8da3d8ddc4ed32989b7b2cbd0
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-03 18:17:37 -0300

    refactor: Changin import order

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
