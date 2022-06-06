81187d39f36dbc9fc5992f4851ebb66f7ae50a3e
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-05 23:46:06 -0300

    feat: Creating helper to register a new user by post method

---
+++ front-end/src/helpers/createUser.js
@@ -0,0 +1,19 @@
+const CryptoJS = require('crypto-js');
+
+export default async function validateUser(data) {
+  const URL = 'http://localhost:3001/user';
+
+  const md5Password = CryptoJS.MD5(data.password).toString();
+
+  const request = await fetch(URL, {
+    method: 'POST',
+    body: JSON.stringify({ ...data, password: md5Password }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+
+  const response = await request.json();
+
+  return response;
+}
