e845af8e4db1134256fe6d27ddf7785f99bfaee1
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-05 23:40:22 -0300

    feat: Creating getById function in user service

--- back-end/src/api/services/userService.js
+++ back-end/src/api/services/userService.js
@@ -4,14 +4,24 @@ class UserService {
   constructor() {
     this.userModel = User;
 
+    this.NOT_FOUND = 'User not found';
 
     this.getAll = this.getAll.bind(this);
+    this.getById = this.getById.bind(this);
   }
 
   async getAll() {
     const users = await this.userModel.findAll();
 
     return { code: 200, users };
+  }
+
+  async getById(id) {
+    const user = await this.userModel.findOne({ where: { id } });
+
+    if (!user) return { code: 404, message: this.NOT_FOUND };
+
+    return { code: 200, user };
   }
 
 }
