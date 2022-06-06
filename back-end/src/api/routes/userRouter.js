f80a6d88da34231a575865b10e7bfecc031a413a
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-05 23:34:46 -0300

    feat: Creating user router and get route

---
+++ back-end/src/api/routes/userRouter.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const UserController = require('../controllers/userController');
+const userMiddleware = require('../middlewares/userMiddleware');
+
+const userController = new UserController();
+
+router.get('/', userController.getAll);
+
+
+module.exports = router;
