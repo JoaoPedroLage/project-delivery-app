75e71a9a4cfec37b2feca35610bfbef15ef2bd5d
thiagoteixas <thiagoteixas@gmail.com>
2022-06-06 18:53:56 -0300

    feat: Adding customer order skeleton

---
+++ back-end/src/api/services/customerOrderService.js
@@ -0,0 +1,19 @@
+const { Sale } = require('../../database/models');
+const TokenGenerator = require('../tokenGenerator');
+
+class CustomerOrderService {
+  constructor() {
+    this.tokenInstance = new TokenGenerator();
+    this.getSale = this.getSale.bind(this);
+  }
+
+  async getSale(userId) {
+    const findSale = await Sale.findAll({ where: { userId } });
+    
+    console.log('🚀 ~ file: customerOrderService.js ~ line 12 ~ CustomerOrderService ~ getSale ~ findSale', findSale);
+
+    return findSale;
+  }
+}
+
+module.exports = CustomerOrderService;
