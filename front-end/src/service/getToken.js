018926983f1de9cc5a0854c972db8ea846907a2b
thiagoteixas <thiagoteixas@gmail.com>
2022-06-06 18:51:40 -0300

    refactor: merge

---
+++ front-end/src/service/getToken.js
@@ -0,0 +1,14 @@
+export default async function getToken(token) {
+  const URL = 'http://localhost:3001/login/validate';
+
+  const request = await fetch(URL, {
+    method: 'GET',
+    headers: {
+      Authorization: `${token}`,
+    },
+  });
+
+  const response = await request.json();
+
+  return response;
+}
