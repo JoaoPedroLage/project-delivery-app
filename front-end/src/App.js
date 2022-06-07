e5b15b6f15444c20347ea76a8165abfe3810e555
thiagoteixas <thiagoteixas@gmail.com>
2022-06-07 17:25:46 -0300

    feat: Creating customer products page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,8 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
+// import CustomerOrdersPage from './pages/CustomerOrdersPage';
+import CustomerProductsPage from './pages/CustomerProductsPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -15,11 +17,19 @@ function App() {
       />
       <Route
         path="/login"
-        element={ <LoginPage /> }
+        element={ <LoginPage replace /> }
       />
       <Route
         path="/register"
         element={ <RegisterPage /> }
+      />
+      {/* <Route
+        path="/customer/orders"
+        element={ <CustomerOrdersPage /> }
+      /> */}
+      <Route
+        path="/customer/products"
+        element={ <CustomerProductsPage /> }
       />
     </Routes>
   );
