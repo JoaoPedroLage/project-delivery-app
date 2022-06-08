edf9d4a8756431d1894d5951854b8190507ba0ce
JoaoPedroLage <JoaoPedro.Lage@outlook.com>
2022-06-08 16:43:33 -0300

    refactor: changes function responsability

--- front-end/src/pages/CustomerProductsPage.js
+++ front-end/src/pages/CustomerProductsPage.js
@@ -1,33 +1,12 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext } from 'react';
 import { Navbar } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import AppContext from '../context/AppContext';
-import getTokenData from '../services/getTokenData';
+// import getTokenData from '../services/getTokenData';
 
 export default function ProductsPage() {
-  const { token } = useContext(AppContext);
-  const [username, setUsername] = useState('');
+  const { name } = useContext(AppContext);
   const navigate = useNavigate();
-
-  useEffect(() => {
-    const profile = async () => {
-      try {
-        const { name } = await getTokenData(token);
-        localStorage.setItem('user', name);
-        setUsername(name);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    const userStorage = localStorage.getItem('user');
-    if (userStorage === null || userStorage === 'undefined') profile();
-    else console.log(typeof userStorage);
-  }, [token]);
-
-  useEffect(() => {
-    const userStorage = localStorage.getItem('user');
-    if (userStorage) setUsername(userStorage);
-  }, []);
 
   return (
     <div>
@@ -39,10 +18,15 @@ export default function ProductsPage() {
           {' '}
           Meus Pedidos
         </a>
-        <a className="navBar-a" href="/register">
-          {username}
+        <a
+          data-testid="data-testid='customer_products__element-navbar-user-full-name'"
+          className="navBar-a"
+          href="/register"
+        >
+          { name }
         </a>
         <a
+          data-testid="customer_products__element-navbar-link-logout"
           className="navBar-a"
           onClick={ () => {
             localStorage.clear();
