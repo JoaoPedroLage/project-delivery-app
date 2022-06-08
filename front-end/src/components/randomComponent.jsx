c987b255a89010ae4165319099a2cdd07ed1b2fb
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-08 19:00:25 -0300

    fix: Lint errors

--- front-end/src/components/randomComponent.jsx
+++ front-end/src/components/randomComponent.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import { Navigate, useRoutes } from 'react-router-dom'
-import LoginPage from '../pages/LoginPage'
-import RegisterPage from '../pages/RegisterPage'
+import React from 'react';
+import { Navigate, useRoutes } from 'react-router-dom';
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage';
 import CustomerProductsPage from '../pages/CustomerProductsPage';
+
 export default function RandomComponent() {
-  return(
+  return (
     useRoutes([
-      {path:'/', element: <Navigate to="/login" replace /> },
-      {path:"/login", element: <LoginPage replace /> },
-      {path:"/register", element: <RegisterPage /> },
-      {path:"/customer/products", element: <CustomerProductsPage /> }
+      { path:'/', element: <Navigate to="/login" replace /> },
+      { path:"/login", element: <LoginPage replace /> },
+      { path:"/register", element: <RegisterPage /> },
+      { path:"/customer/products", element: <CustomerProductsPage /> },
     ])
-  )
+  );
 }
