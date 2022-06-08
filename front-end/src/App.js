f686e4247c9df926f0bfc54916f115abda6148d6
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-08 18:29:08 -0300

    feat: Changes routes import

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,37 +1,17 @@
 import './App.css';
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import RegisterPage from './pages/RegisterPage';
+// import LoginPage from './pages/LoginPage';
+// import RegisterPage from './pages/RegisterPage';
 // import CustomerOrdersPage from './pages/CustomerOrdersPage';
 import CustomerProductsPage from './pages/CustomerProductsPage';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import RandomComponent from './components/randomComponent';
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/"
-        element={ <Navigate to="/login" replace /> }
-      />
-      <Route
-        path="/login"
-        element={ <LoginPage replace /> }
-      />
-      <Route
-        path="/register"
-        element={ <RegisterPage /> }
-      />
-      {/* <Route
-        path="/customer/orders"
-        element={ <CustomerOrdersPage /> }
-      /> */}
-      <Route
-        path="/customer/products"
-        element={ <CustomerProductsPage /> }
-      />
-    </Routes>
+      <RandomComponent />
   );
 }
 
