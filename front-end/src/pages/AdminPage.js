ea4faf6d7b834d4b9a3ef863a73fd97fe77d4070
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-14 14:39:48 -0300

    feat: Creates admin page with form components

---
+++ front-end/src/pages/AdminPage.js
@@ -0,0 +1,103 @@
+import React, { useContext, useState } from 'react';
+import NameInput from '../components/administrator/NameInput';
+import EmailInput from '../components/administrator/EmailInput';
+import PasswordInput from '../components/administrator/PasswordInput';
+import RoleInput from '../components/administrator/RoleInput';
+import AppContext from '../context/AppContext';
+import createUser from '../services/apiCreate';
+
+export default function AdminPage() {
+  const {
+    name, setName, email, setEmail, password, setPassword, role, setRole,
+  } = useContext(AppContext);
+  const [invalidRegister, setInvalidRegister] = useState(false);
+  const nameInStorage = JSON.parse(localStorage.getItem('user'));
+
+  function validateRegister() {
+    const emailValidationRegex = /\S+@\S+\.\S+/;
+    const MIN_PASSWORD = 6;
+    const MIN_NAME = 12;
+    const passwordValidation = password.length >= MIN_PASSWORD && password.length >= 1;
+    const nameValidation = name.length >= MIN_NAME;
+    const validatingEmail = emailValidationRegex.test(email);
+    const inputValidation = (passwordValidation && nameValidation && validatingEmail);
+    return !inputValidation;
+  }
+
+  async function onSubmitUser(e) {
+    e.preventDefault();
+
+    console.log({ name, email, password, role });
+
+    const newUser = await createUser({ name, email, password, role }, 'user');
+    if (newUser.message) {
+      setInvalidRegister(true);
+    }
+    console.log(newUser);
+    setName('');
+    setEmail('');
+    setPassword('');
+    setRole('customer');
+  }
+
+  return (
+    <div>
+      <nav className="navBar">
+        <a
+          className="navBar-a"
+          href="/customer/checkout"
+        >
+          Gerenciar Usuários
+        </a>
+        <a
+          className="navBar-a"
+          href="/register"
+        >
+          {!nameInStorage
+            ? name
+            : nameInStorage.name}
+        </a>
+        <a
+          className="navBar-a"
+          onClick={ () => {
+            localStorage.clear();
+            navigate('/login');
+          } }
+          href="/login"
+        >
+          Sair
+        </a>
+      </nav>
+      <div className="register-page-container" onSubmit={ (e) => onSubmitUser(e) }>
+        <div className="register-items-container">
+          Cadastrar novo usuário
+          <form className="register-form">
+            <NameInput />
+            <EmailInput />
+            <PasswordInput />
+            <RoleInput />
+            <button
+              data-testid="admin_manage__button-register"
+              type="submit"
+              disabled={ validateRegister() }
+            >
+              Cadastrar
+            </button>
+          </form>
+          {
+            invalidRegister
+              ? (
+                <p
+                  data-testid="common_register__element-invalid_register"
+                >
+                  Usuário já existente
+                </p>
+              )
+              : null
+          }
+        </div>
+      </div>
+
+    </div>
+  );
+}
