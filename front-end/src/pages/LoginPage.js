329db66e4dcd0050dcbe0dff0e971fcaead97fdc
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-21 18:33:33 -0300

    feat: adds logic to clear localstorage

--- front-end/src/pages/LoginPage.js
+++ front-end/src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { AiOutlineEye, AiOutlineEyeInvisible } from 'react-icons/ai';
 import getToken from '../services/getToken';
@@ -62,6 +62,9 @@ export default function LoginPage(/* { history } */) {
       setInvalidUser(true);
     }
   }
+  useEffect(() => {
+    localStorage.clear();
+  }, []);
 
   return (
     <div className="login-page-container">
