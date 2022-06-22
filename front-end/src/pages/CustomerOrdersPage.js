f930adf219002b880dc6cf7f18e5e1989cab8432
Alessandro Fuhr de Mello <ale_95_f.mello@hotmail.com>
2022-06-21 22:27:27 -0300

    feat: Creates customer order page

--- front-end/src/pages/CustomerOrdersPage.js
+++ front-end/src/pages/CustomerOrdersPage.js
@@ -1,81 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import apiGetAll from '../services/apiGetAll';
 import Header from '../components/Header';
 
 export default function CustomerOrdersPage() {
-  // const { token } = useContext(AppContext);
+  const [allSales, setAllSales] = useState([]);
 
-  // async function decodeUserToken() {
-  //   const response = await getData(token);
-  // }
+  async function getAllSales() {
+    const user = JSON.parse(localStorage.getItem('user'));
+    const sales = await apiGetAll(`sales/customer/${user.id}`);
+    setAllSales([...sales]);
+  }
+
+  useEffect(() => {
+    getAllSales();
+  }, []);
 
   return (
     <div>
-    <div className="customer-orders-page-container">
+      <div className="customer-orders-page-container">
         <Header />
-      <div className="customer-orders-main-container">
-        { decodeUserToken() }
-        <CardGroup>
-          <OrderCard />
-        </CardGroup>
+        <div className="customer-orders-main-container">
+          {allSales.map((sale, index) => (
+            <div
+              key={ index }
+            >
+              <p
+                data-testid={ `customer_orders__element-order-id-${sale.id}` }
+              >
+                {sale.id}
+              </p>
+              <p
+                data-testid={ `customer_orders__element-delivery-status-${sale.id}` }
+              >
+                {sale.status}
+              </p>
+              <p
+                data-testid={ `customer_orders__element-order-date-${sale.id}` }
+              >
+                {Date(sale.saleDate)}
+              </p>
+              <p
+                data-testid={ `customer_orders__element-card-price-${sale.id}` }
+              >
+                {sale.totalPrice}
+              </p>
+            </div>
+          ))}
 
         </div>
       </div>
     </div>
   );
 }
-// // import React, { useContext, useEffect, useRef } from 'react';
-// // import React, { useContext } from 'react';
-// import React from 'react';
-// import { CardGroup } from 'react-bootstrap';
-// import OrderCard from '../components/OrderCard';
-// // import AppContext from '../context/AppContext';
-// // import getData from '../services/getData';
-// // import apiGetById from '../services/apiGetById';
-
-// export default function CustomerOrdersPage() {
-//   const { token } = useContext(AppContext);
-//   const teste = useRef();
-
-//   function decodeUserToken() {
-//     console.log(teste);
-//     return <div>teste</div>;
-//     // const { token } = useContext(AppContext);
-
-//     async function decodeUserToken() {
-//     // const response = await getData(token);
-//     }
-
-//     useEffect(() => {
-//       const test = async () => {
-//         try {
-//           const response = await getData(token);
-
-//           console.log(response);
-//           console.log(response.id);
-
-//           const result = await apiGetById(response.id, 'customer/orders');
-
-//           console.log(result);
-
-//           return response;
-//         } catch (error) {
-//           console.log(error, 'aa');
-//         }
-//       };
-//       teste.current = test().then((value) => value);
-//     });
-
-//     return (
-//       <div className="customer-orders-page-container">
-//         <div className="customer-orders-topbar-container">
-//           {/* Não sei se alguém já fez essa parte então vou deixar em branco por agora */}
-//         </div>
-//         <div className="customer-orders-main-container">
-//           <CardGroup>
-//             <OrderCard />
-//           </CardGroup>
-//           { decodeUserToken() }
-//         </div>
-//       </div>
-//     );
-//   }
-// }
